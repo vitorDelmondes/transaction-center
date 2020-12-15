f7c4d2a0a05228f9a467d408be9b1b760fe71265
Vitor Delmondes <vitordelmondes00@gmail.com>
2020-12-14 23:45:32 -0300

    Estrutura.

--- App.tsx
+++ App.tsx
@@ -2,20 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './src/pages/Home';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" backgroundColor="#F2F2F3" translucent/>
+      <Home/>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
